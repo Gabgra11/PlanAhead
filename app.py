7bac431c715be80914709012f2ee548c0159a164
Gabriel Grais <gabrielgrais11@gmail.com>
2023-07-31 16:54:08 -0400

    Move connection logic to db.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 '''
 TODO: Turn off edit mode on all notes if background is clicked.
-TODO: Move db connection code to db.py
 TODO: Fix formatting on edit view
 TODO: Fix note date string format
 TODO: Implement search
@@ -13,17 +12,11 @@ TODO: Switch from SQLite to PostgreSQL
 from flask import Flask, render_template, request
 from scripts import note, parser, tag, search, db
 from config import *
-import sqlite3
 
 app = Flask(__name__)
 
 notes_list = []
 tags_list = []
-
-def get_db_connection():
-    conn = sqlite3.connect('db/database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -31,9 +24,8 @@ def home():
     global tags_list
 
     filter_list = []
-    conn = get_db_connection()
-    notes_list = db.get_notes_list(conn)
-    tags_list=db.get_tags_list(conn)
+    notes_list = db.get_notes_list()
+    tags_list=db.get_tags_list()
 
     if request.method == "POST":
         match request.form["post_type"]:
@@ -43,12 +35,12 @@ def home():
                 tag, trimmed_title = parser.parse_title_for_tag(title, tags_list)
                 date, trimmed_title = parser.parse_title_for_date(trimmed_title, date_delimiter)
                 n = note.Note(title=trimmed_title, body="", tag=tag, date=date)  # TODO: Add body
-                n.id = db.add_new_note(conn, n)
+                n.id = db.add_new_note(n)
                 notes_list.append(n)
 
             case "filter_by_tag":
                 new_filter_id = request.form['filter_tag']
-                filter_list.append(db.get_tag_by_id(conn, new_filter_id))
+                filter_list.append(db.get_tag_by_id(new_filter_id))
                 notes_list = search.filter_by_tags(notes_list, filter_list)
 
             case "edit_mode": # Put a note into editing mode
@@ -60,32 +52,31 @@ def home():
             case "edit_note":
                 note_id = request.form['note_id']
                 new_title = request.form['new_title']
-                new_tag = db.get_tag_by_id(conn, request.form['new_tag'])
+                new_tag = db.get_tag_by_id(request.form['new_tag'])
                 new_body = request.form['new_body']
                 new_date = request.form['new_date']
                 new_note = note.Note(new_title, new_body, new_tag, note_id, new_date)
-                db.update_note(conn, note_id, new_note)
+                db.update_note(note_id, new_note)
                 notes_list = note.Note.update_notes_list(notes_list, note_id, new_note)
 
             case "delete_note":
                 note_id = request.form['note_id']
                 notes_list = note.Note.remove_note_from_list(notes_list, note_id)
-                db.remove_note(conn, note_id)
+                db.remove_note(note_id)
 
-    return render_template("index.html", notes=notes_list, filter_list=filter_list, tags_list=db.get_tags_list(conn))
+    return render_template("index.html", notes=notes_list, filter_list=filter_list, tags_list=db.get_tags_list())
 
 @app.route("/tags", methods=["GET", "POST"])
 def tags():
     global tags_list
 
-    conn = get_db_connection()
     if request.method == 'POST':
         match request.form['post_type']:
             case "new_tag":
                 tag_name = request.form['tag_name']
                 if len(tag_name) > 0:   # Prevent blank tag names
                     t = tag.Tag(tag_name)
-                    new_tag_id = db.add_new_tag(conn, t)
+                    new_tag_id = db.add_new_tag(t)
                     if new_tag_id:
                         t.id = new_tag_id
                         tags_list.append(t)
@@ -100,14 +91,14 @@ def tags():
                 if len(new_tag_name) > 0:   # Prevent blank tag names
                     new_tag = tag.Tag(new_tag_name, new_bg_color, tag_id)
                     tags_list = tag.Tag.update_tags_list(tags_list, tag_id, new_tag)
-                    db.update_tag(conn, tag_id, new_tag)
+                    db.update_tag(tag_id, new_tag)
 
             case "delete_tag":
                 tag_id = request.form['tag_id']
                 tags_list = tag.Tag.remove_tag(tags_list, tag_id)
-                db.remove_tag_from_list(conn, tag_id)
+                db.remove_tag_from_list(tag_id)
 
-    return render_template("tags.html", bg_colors=tag.Tag.bg_colors, tags_list=db.get_tags_list(conn))
+    return render_template("tags.html", bg_colors=tag.Tag.bg_colors, tags_list=db.get_tags_list())
 
 if __name__ == "__main__":
     app.run()
--- scripts/db.py
+++ scripts/db.py
@@ -1,7 +1,14 @@
 from scripts import note, tag, search
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('db/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 # Returns the ID of the new note
-def add_new_note(conn, n):
+def add_new_note(n):
+    conn = get_db_connection()
     command_beginning = 'INSERT INTO notes ('
     command_end = 'VALUES ('
     values = []
@@ -44,7 +51,8 @@ def add_new_note(conn, n):
     # return new_note_id
 
 # Returns the ID of the new tag, or None if tag name is a duplicate:
-def add_new_tag(conn, t):
+def add_new_tag(t):
+    conn = get_db_connection()
     existing_tags = get_tags_list(conn)
     if search.validate_new_tag_name(existing_tags, t):
         command = 'INSERT INTO tags (tag_name, bg_color) VALUES (?, ?)'
@@ -56,11 +64,13 @@ def add_new_tag(conn, t):
         print("Tag name '%s' is invalid" %(t.tag_name))
         return None
 
-def get_note_by_id(conn, nid):
+def get_note_by_id(nid):
+    conn = get_db_connection()
     command = 'SELECT * from notes where id = ?'
     return conn.execute(command, (str(nid),)).fetchone()
 
-def get_tag_by_id(conn, tid):
+def get_tag_by_id(tid):
+    conn = get_db_connection()
     command = 'SELECT * from tags WHERE id = ?'
     query = conn.execute(command, (str(tid),)).fetchone()
     if query:
@@ -73,7 +83,8 @@ def get_tag_by_id(conn, tid):
         return None
 
 # Replaces the note with the given note id with the contents of new_note
-def update_note(conn, nid, new_note):
+def update_note(nid, new_note):
+    conn = get_db_connection()
     command = 'UPDATE notes SET '
     values = []
 
@@ -101,7 +112,8 @@ def update_note(conn, nid, new_note):
     conn.commit()
 
 # Replaces the tag with the given tag id with the contents of new_tag
-def update_tag(conn, tid, new_tag):
+def update_tag(tid, new_tag):
+    conn = get_db_connection()
     if search.validate_new_tag_name(get_tags_list(conn), new_tag):
         command = 'UPDATE tags SET tag_name = ?, bg_color = ? WHERE id = ?'
         conn.execute(command, (str(new_tag.tag_name), str(new_tag.bg_color), str(tid)))
@@ -110,17 +122,20 @@ def update_tag(conn, tid, new_tag):
         # TODO: Handle invalid tag name
         print("Tag name '%s' is a duplicate" %(new_tag.tag_name))
 
-def remove_note(conn, nid):
+def remove_note(nid):
+    conn = get_db_connection()
     command = 'DELETE From notes WHERE id = ?'
     conn.execute(command, (str(nid),))
     conn.commit()
 
-def remove_tag(conn, tid):
+def remove_tag(tid):
+    conn = get_db_connection()
     command = 'DELETE from tags WHERE id = ?'
     conn.execute(command, (str(tid),))
     conn.commit()
 
-def get_notes_list(conn):
+def get_notes_list():
+    conn = get_db_connection()
     query = conn.execute('SELECT * FROM notes n LEFT JOIN tags t ON n.tag = t.id;').fetchall()
     notes_list = []
     for r in query:
@@ -136,7 +151,8 @@ def get_notes_list(conn):
         notes_list.append(note.Note(title, body, note_tag, nid, date=date))
     return notes_list
 
-def get_tags_list(conn):
+def get_tags_list():
+    conn = get_db_connection()
     query = conn.execute('SELECT * FROM tags;').fetchall()
     tags_list = []
     for r in query:
